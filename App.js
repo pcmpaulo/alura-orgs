f8a9761039a2bd26d7de65e3bc967d1352ed5df1
Correa <pcmpaulo7@gmail.com>
2023-03-16 19:55:39 -0300

    Feat(Basket and app): Add external font and styles to basket component

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { StatusBar, StyleSheet, SafeAreaView } from 'react-native';
+import {StatusBar, StyleSheet, SafeAreaView, View} from 'react-native';
 import Basket from "./scr/components/Basket";
+import {useFonts, Montserrat_400Regular, Montserrat_700Bold} from "@expo-google-fonts/montserrat";
+
 
 // SafeAreaView: Prevent to write on status bar for IOS
 // StatusBar: Create a padding preventing the application to render on status bar for Android (create above the components)
 
+
 export default function App() {
-  return (
-  <SafeAreaView style={styles.container}>
-      <StatusBar/>
-      <Basket></Basket>
-    </SafeAreaView>
+    //Use external font
+    const [externalFont] = useFonts({
+        "MontserratRegular": Montserrat_400Regular,
+        "MontserratBold": Montserrat_700Bold
+    })
+    if (!externalFont) { // Return empty view if font is not loaded
+        return <View></View>
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar/>
+            <Basket></Basket>
+        </SafeAreaView>
   );
 }
 
